083a0d8277f36c8ebf416af2687e66541574921a
Xiaoquan Jiang <xij053@UCSD.EDU>
2020-01-23 15:21:19 -0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("What did you click on?");
+		$("#testjs").text("Don't click again...");
+		$(".jumbotron p").addClass("active");
+
 	});
 
+	$(".project").click(projectClick)
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick() {
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle(400)
+    }
+	e.preventDefault();
+
 }
